58250dacba96ee06ce94307baf2efe32e7deb57c
vaibhavs0007 <137402650+vaibhavs0007@users.noreply.github.com>
2025-08-04 12:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 provider "aws" {
   region = var.region
 }
@@ -55,20 +54,6 @@ resource "aws_iam_instance_profile" "ec2_ssm_profile" {
   role = aws_iam_role.ec2_ssm_role.name
 }
 
-resource "aws_instance" "web" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  subnet_id              = var.subnet_id
-  security_groups        = [aws_security_group.web_sg.id]
-  user_data              = file("user_data.sh")
-  iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
-
-  tags = {
-    Name = "Apache2-PHP-EC2"
-  }
-}
-
 resource "aws_kms_key" "rds_kms" {
   description             = "KMS for Secrets Manager rotation"
   deletion_window_in_days = 7
@@ -84,6 +69,132 @@ resource "aws_secretsmanager_secret_rotation" "rds_rotation" {
   }
 }
 
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "web-lt"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  user_data = base64encode(file("user_data.sh"))
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_ssm_profile.name
+  }
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "Apache2-PHP-ASG"
+    }
+  }
+}
+
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = var.public_subnets
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_autoscaling_group" "web_asg" {
+  name                      = "web-asg"
+  max_size                  = 3
+  min_size                  = 1
+  desired_capacity          = 1
+  vpc_zone_identifier       = var.public_subnets
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.web_template.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "Apache2-PHP-ASG"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_cloudfront_distribution" "cf_dist" {
+  origin {
+    domain_name = aws_lb.web_alb.dns_name
+    origin_id   = "alb-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "alb-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "Web-CF-Distribution"
+  }
+}
 
 resource "aws_db_subnet_group" "rds_subnet" {
   name       = "rds-subnet-group"
